fbe35f1f60a40f0569cf2c53c56091ded3a13068
Olympic-Powerhouse <olympicpowerhouse@gmail.com>
2024-04-22 23:08:31 +0800

    idk

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,23 +8,23 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
+    pageTitle: "Olympic Powerhouse",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "olympic-powerhouse.github.io",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
       fontOrigin: "googleFonts",
       cdnCaching: true,
       typography: {
-        header: "Schibsted Grotesk",
-        body: "Source Sans Pro",
-        code: "IBM Plex Mono",
+        header: "Josefin Sans",
+        body: "Itim",
+        code: "Major Mono Display",
       },
       colors: {
         lightMode: {
