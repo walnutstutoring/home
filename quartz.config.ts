6db766f8094f8889e0b1377b806df6530b5ed6be
Olympic-Powerhouse <olympicpowerhouse@gmail.com>
2024-04-23 14:45:21 +0800

    Quartz sync: Apr 23, 2024, 2:45 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -15,7 +15,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "olympic-powerhouse.github.io/quartz",
+    baseUrl: "olympic-powerhouse.github.io/main",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
